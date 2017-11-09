c2ab6696b7c21cafb3b645c95a288ff272ae18db
cheriejw <cheriejawoo@yahoo.com>
2017-11-08 19:42:53 -0800

    Pushing initial tutorial completion.

---
+++ CSSOAPAPI/TestWebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebApplication.Models;
+
+//In Web API controller object handles HTTP requests. This returns a list or single product specified by ID.
+namespace TestWebApplication.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Catergory = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Catergory = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Catergory = "Hardware", Price = 16.99M },
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
