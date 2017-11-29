e7cd615f420b759abadb214edd118c99bf75d9d9
cheriejw <cheriejawoo@yahoo.com>
2017-11-29 00:54:13 -0800

    Code complete, exe on docker container left.

--- CSharp/CSMICROSERVICE/WeatherMicroservice/Startup.cs
+++ CSharp/CSMICROSERVICE/WeatherMicroservice/Startup.cs
@@ -7,9 +7,58 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace WeatherMicroservice
 {
+    public static class Extensions
+    {
+        //Parsing the Query String.
+        //bool TryParse(string s, out double result);
+        public static double? TryParse(this string input)
+        {
+            double result;
+            if (double.TryParse(input, out result))
+            {
+                return result;
+            }
+            else
+            {
+                return default(double?);
+            }
+        }
+    }
+
+    //data container object to hold values for weather forcast
+    public class WeatherReport
+    {
+        //Constructor for random generation
+        public WeatherReport(double latitude, double longitude, int daysInFuture)
+        {
+            var generator = new Random((int)(latitude + longitude) + daysInFuture);
+
+            HiTemperature = generator.Next(40, 100);
+            LoTemperature = generator.Next(0, HiTemperature);
+            AverageWindSpeed = generator.Next(0, 45);
+            Conditions = PossibleConditions[generator.Next(0, PossibleConditions.Length - 1)];
+        }
+
+        private static readonly string[] PossibleConditions = new string[]
+        {
+            "Sunny",
+            "Mostly Sunny",
+            "Partly Sunny",
+            "Partly Cloudy",
+            "Mostly Cloudy",
+            "Rain"
+        };
+
+        public int HiTemperature { get; }
+        public int LoTemperature { get; }
+        public int AverageWindSpeed { get; }
+        public string Conditions { get; }
+    }
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -28,9 +77,28 @@ namespace WeatherMicroservice
                 app.UseDeveloperExceptionPage();
             }
 
+            //context is the HttpContext for request
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //http://localhost:5000/?lat=-35.55&long=-12.35
+                String latString = context.Request.Query["lat"].FirstOrDefault(); //StringValue type, FOD() returns String
+                String longString = context.Request.Query["long"].FirstOrDefault();
+
+                var latitude = latString.TryParse();
+                var longitude = longString.TryParse();
+
+                if (latitude.HasValue && longitude.HasValue)
+                {
+                    var forecast = new List<WeatherReport>();
+                    for (var days = 1; days < 6; days++)
+                    {
+                        forecast.Add(new WeatherReport(latitude.Value, longitude.Value, days));
+                    }
+                    var json = JsonConvert.SerializeObject(forecast, Formatting.Indented);
+                    context.Response.ContentType = "application/json; charset=utf-8";
+                    await context.Response.WriteAsync(json);
+                }
+                //await context.Response.WriteAsync($"Weather for lat: {latitude}, long: {longitude}");
             });
         }
     }
